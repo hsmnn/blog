19f61ec956da415184d2a2c974822a42cd8680c8
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-03-26 22:57:54 +0100

    step 10 done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,21 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated"
+            redirect_to article_path(@article)
+        else
+            flash[:notice] = "Article was not updated"
+            render 'edit'
+        end
+    end
+
     private
         def article_params
             params.require(:article).permit(:title, :description)
