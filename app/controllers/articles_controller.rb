65f3a9c01de2c36d6250547664210be9f8294b5c
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-03-28 12:56:37 +0100

    stop 11, 12, 13 and 14 done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article, only [:edit, :update, :show, :destroy]
 
     def new
         @article = Article.new
@@ -15,15 +17,12 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was updated"
             redirect_to article_path(@article)
@@ -37,9 +36,19 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def destroy
+        @article.destroy
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path
+    end
+
     private
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+
+        def set_article
+            @article = Article.find(params[:id])
         end
 
 end
