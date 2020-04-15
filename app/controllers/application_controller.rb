cb146fd48109f1d09018f669120957b7cb1e2d77
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-04-15 18:09:33 +0200

    blog finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
         if !logged_in?
             flash[:danger] = "You must be logged in to perform that action"
             redirect_to root_path
+        end
     end
 end
